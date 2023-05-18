0865208882bdfec33a10b3d7c15f2e615d8fb125
alsuddlek1 <minyoung99311@gmail.com>
2023-05-18 16:39:46 +0900

    123

--- final-pjt-FRONT/src/App.vue
+++ final-pjt-FRONT/src/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <div id="app">
-    <nav class="navbar bg-body-tertiary ">
-    <form class="container-fluid justify-content-center">
-      <button class="btn btn-outline-success ms-3 me-3" type="button"><router-link :to="{name : 'MovieMainView'}">Main</router-link></button>
-      <button class="btn btn-outline-success ms-3 me-3" type="button" v-if="isLogin === false"><router-link :to="{name : 'signUpView'}">signup</router-link> </button>
-      <button class="btn btn-outline-success ms-3 me-3" type="button" v-if="isLogin === false"><router-link :to="{name : 'LoginView'}">logIn</router-link> </button>
-      <button class="btn btn-outline-success ms-3 me-3" type="button" v-if="isLogin !== false"><router-link :to="{name : 'MovieMainView'}" @click.native="logOut">logOut</router-link></button> 
-      </form>
+  <div id="header">
+    <div class="logo">
+      <router-link :to="{name : 'MovieMainView'}">
+        <!-- 메인 페이지로 이동 -->
+        <span class="icon fa-gem"></span> 
+      </router-link>
+    </div>
+    <div class="content">
+      <div class="inner">
+        <h1>Dimension</h1>
+        <p v-if="isLogin === false">
+        ---의 서비스를 이용할 준비가 되셨나요? 그렇다면
+        <router-link :to="{name : 'LoginView'}">로그인</router-link>을 하세요 !
+        </p>
+        <p v-if="isLogin === false">
+        회원이 아니라면 
+        <router-link router-link :to="{name : 'signUpView'}">여기</router-link>를 통해
+        <router-link router-link :to="{name : 'signUpView'}">회원가입</router-link>을 하세요
+        </p>
+
+        <p v-if="isLogin !== false">
+          <router-link :to="{name : 'MovieMainView'}" @click.native="logOut">logOut</router-link>
+        </p>
+      </div>
+    </div>
+    <nav class="navbar bg-body-tertiary " v-if="isLogin !== false">
+      <ul>
+        <li><a href="#intro">INTRO</a></li>
+        <li><router-link :to="{name : 'randomRecommend'}">TODAY MOVIE</router-link></li>
+        <li><router-link :to="{name : 'movies'}">MOVIE LIST</router-link></li>
+        <li><router-link :to="{name : 'PopularMovie'}">POPULAR MOVIE</router-link></li>
+        <li><router-link :to="{name : 'LikeMovie', params: {user: this.$store.state.accounts.userpk} }">LIKED MOVIE</router-link></li>
+      </ul>
     </nav>
-      
- 
     <router-view/>
   </div>
 </template>
--- final-pjt-FRONT/src/views/MovieMainView.vue
+++ final-pjt-FRONT/src/views/MovieMainView.vue
@@ -1,15 +1,5 @@
 <template>
-  <div class="text-on-img">
-    <div class="background-wrap">
-      <div class="content">
-        <span>안녕하세요</span>
-        <!-- <img src="@/assets/main_movie2.jpg" class="image"> -->
-      </div>
-    </div>
-        <router-link :to="{name : 'movies'}">영화 전체 리스트 보기</router-link><br>
-        <router-link :to="{name : 'LikeMovie', params: {user: this.$store.state.accounts.userpk}}">내가 좋아요한 영화</router-link><br>
-        <router-link :to="{name : 'PopularMovie'}">인기 영화 추천</router-link><br>
-        <router-link :to="{name : 'randomRecommend'}">오늘의 추천 영화</router-link>
+  <div>
   </div>
 </template>
 
@@ -20,32 +10,4 @@ export default {
 </script>
 
 <style>
-.background-wrap {
-  width: 1900px;
-  height: 800px;
-  background-image: url('@/assets/main_movie2.jpg');
-  background-size: cover;
-  /* max-width: 100%;
-  max-height: 100%; */
-  justify-content: center;
-  align-items: center;
-}
-
-.content{
-  display: felx;
-  flex-direction: column;
-}
-
-.content span{
-  color: white;
-}
-
-.content span:nth-child(1){
-  font-size: 25px;
-  font-weight: bold;
-}
-
-.content span:nth-child(2){
-  font-size: 20px;
-}
 </style>
