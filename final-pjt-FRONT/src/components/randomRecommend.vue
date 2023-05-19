7eb0b2480f40f699e003dc83f65e55477a64b3cf
신종혁 <shinjh4903@gmail.com>
2023-05-19 17:50:03 +0900

    추천 영화 css수정

--- final-pjt-FRONT/src/components/randomRecommend.vue
+++ final-pjt-FRONT/src/components/randomRecommend.vue
@@ -2,14 +2,13 @@
 <div class="row row-cols-1 row-cols-sm-3 g-2">
 <div class="container">
   <div class="row">
-  <div class="card mouse" @click="goDetail(movies)">
+  <div class="card mouse bg-body bg-opacity-10" @click="goDetail(movies)">
     <img :src="url" class="card-img-top">
       <div class="card-body">
-      <h5 class="card-title">{{movies.title}}</h5>
-      <div class="card-text" style="text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 4; -webkit-box-orient: vertical; overflow: hidden;">{{movies.overview}}</div>
-      <div class="card-text">{{movies.vote_average}}</div>
-      <div class="card-text">{{movies.release_date}}</div>
-      <div class="card-text">{{movies.runtime}}</div>
+      <h5 class="card-title fontcolor font">{{movies.title}}</h5>
+      <div class="card-text fontcolor font" style="text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 4; -webkit-box-orient: vertical; overflow: hidden;">{{movies.overview}}</div>
+      <div class="card-text fontcolor font">{{movies.vote_average}}</div>
+      <div class="card-text fontcolor font">{{movies.release_date}}</div>
     </div>  
   </div>
   </div>    
