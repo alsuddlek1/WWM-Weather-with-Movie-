9e9136aa4d70cd676acf41ef7e9758c9f2352d8d
신종혁 <shinjh4903@gmail.com>
2023-05-22 11:40:12 +0900

    날씨 한글로

--- final-pjt-FRONT/src/App.vue
+++ final-pjt-FRONT/src/App.vue
@@ -46,11 +46,17 @@
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      weather : null,
+
+    }
+  },
   computed: {
     isLogin() {
       return this.$store.getters.isLogin
     },
-    weather() {
+    weathers() {
       return this.$store.state.weather.weather
     }
   },
@@ -65,6 +71,22 @@ export default {
 
   created() {
     this.getMovieWeather()
+
+    if (this.weathers === 'Clouds') {
+      this.weather = '흐린 날'
+    } else if (this.weathers === 'Rain') {
+      this.weather = '비 오는 날'
+    } else if (this.weathers === 'Thunderstorm') {
+      this.weather = '번개치는 날'
+    } else if (this.weathers === 'Drizzle') {
+      this.weather = '이슬비 내리는 날'
+    } else if (this.weathers === 'Snow') {
+      this.weather = '눈 오는 날'
+    } else if (this.weathers === 'Atmosphere') {
+      this.weather = '안개 낀 날'
+    } else if (this.weathers === 'Clear') {
+      this.weather = '맑은 날'
+    } 
   }
   
 }
