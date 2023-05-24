5ddc93a2f75edddedfe99fdc2782d8cce9711b7a
alsuddlek1 <minyoung99311@gmail.com>
2023-05-24 11:51:16 +0900

    수정수정

--- final-pjt-FRONT/src/views/StartWorldCupView.vue
+++ final-pjt-FRONT/src/views/StartWorldCupView.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
     <br>
+    <div>
+
     <h3>FAVORITE MOVIE</h3>
+    <h5><small>영화 월드컵을 통해 가장 좋아하는 영화를 찾아보세요</small></h5>
+    </div>
     <br>
-    <h5 @click="goWorldCup" class="mouse border-bottom"><small class="line">▶ 64강 시작하기</small></h5>
-    <h5 @click="goWorldCup32" class="mouse border-bottom"><small class="line">▶ 32강 시작하기</small></h5>
+
+    <div class="border_size_wc">
+        <span class="border_size_wc2">
+    <h5 @click="goWorldCup" class="mouse border-bottom line"><small>▶ 64강 시작하기</small></h5>
+    <h5 @click="goWorldCup32" class="mouse border-bottom line"><small>▶ 32강 시작하기</small></h5>
+        </span>
+    </div>
  
     <br>
     <br>
@@ -31,5 +40,13 @@ export default {
 </script>
 
 <style>
+.border_size_wc{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
 
+.border_size_wc2{
+    width: 300px;
+}
 </style>
