682585f9aca0431472aee55074208bc5e0760639
minyoung <minyoung99311@gmail.com>
2024-09-29 21:01:42 +0900

    :fire: 불필요한 파일 제거
    불필요한 파일 제거

--- final-react/src/store/movieStore.js
+++
@@ -1,8 +0,0 @@
-import create from 'zustand'
-
-const useMovieStore = create((set) => ({
-    movieData: null,
-    setMovieData: (data) => set(()=>({movieData:data}))
-}))
-
-export default useMovieStore
